9fdec76794fb937fe7a13eac7605d76d15fe3b65
temiscripts <mokhefue@gmail.com>
2025-10-29 14:19:24 +0100

    conversation and message model

--- chats/models.py
+++ chats/models.py
@@ -23,3 +23,23 @@ class User(AbstractUser):
     def __str__(self):
         return f"{self.email} ({self.role})"
 
+class Conversation(models.Model):
+    conversation_id = models.UUIDField(primary_key=True, default=uuid4, editable=False, unique=True)
+    participants = models.ManyToManyField(User, related_name="conversations")
+    created_at = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        participants = ", ".join(user.username for user in self.participants.all()[:3])
+        return f"Conversation ({participants})"
+
+
+
+class Message(models.Model):
+    message_id = models.UUIDField(primary_key=True, default=uuid4, editable=False, unique=True)
+    sender = models.ForeignKey(User, on_delete=models.CASCADE, related_name="messages_sent")
+    conversation = models.ForeignKey(Conversation, on_delete=models.CASCADE, related_name="messages")
+    message_body = models.TextField()
+    sent_at = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return f"From {self.sender.username}: {self.message_body[:30]}..."
